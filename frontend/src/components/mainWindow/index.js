c17626d48ccbf6cdc0ac6a51aa1df23cf8e3a670
Gus Heptig <gusheptig@gmail.com>
2020-10-28 14:50:27 -0400

    created mainwindow for todo items...WIP

--- frontend/src/components/mainWindow/index.js
+++ frontend/src/components/mainWindow/index.js
@@ -31,15 +31,80 @@ const todoItems = [
 export default class index extends Component {
 
     state = {
-
+        viewCompleted: false,
+        todoList: todoItems
     }
+
+    displayCompleted = (status) => {
+        if (status) {
+            return this.setState({ viewCompleted: true});
+        }
+        return this.setState({ viewCompleted: false});
+    }
+
+    renderTabList = () => {
+        return (
+            <div className="my-5 tab-list">
+                <span 
+                onClick={() => this.displayCompleted(true)}
+                className={this.state.viewCompleted ? "active" : ""}
+                >
+                    Complete
+                </span>
+                <span
+                onClick={() => this.displayCompleted(false)}
+                className={this.state.viewCompleted ? "" : "active"}
+                >
+                    Incomplete
+                </span>
+            </div>
+        )
+    }
+
+    renderItems = () => {
+       const {viewCompleted} = this.state;
+       const newItems = this.state.todoList.filter((item) => item.completed == viewCompleted);
+
+       return newItems.map((item) => (
+           <li 
+           key={item.id} 
+           className="list-group-item d-flex justify-content-between align-items-center"
+           >
+            <span
+              className={`todo-title mr-2 ${
+                this.state.viewCompleted ? "completed-todo" : ""
+              }`}
+              title={item.description}
+            >
+              {item.title}
+            </span>
+            <span>
+              <button className="btn btn-secondary mr-2"> Edit </button>
+              <button className="btn btn-danger">Delete </button>
+            </span>
+           </li>
+       ));
+      };
 
 
     render() {
         return (
-            <>
-                <h1 className="text-white">Main Window</h1>
-            </>
-        )
+            <main className="content">
+            <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
+            <div className="row ">
+              <div className="col-md-6 col-sm-10 mx-auto p-0">
+                <div className="card p-3">
+                  <div className="">
+                    <button className="btn btn-primary">Add Task</button>
+                  </div>
+                  {this.renderTabList()}
+                  <ul className="list-group list-group-flush">
+                    {this.renderItems()}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </main>
+          );
     }
 }
